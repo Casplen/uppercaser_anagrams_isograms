94015a57fbdcd16ae271596979e8d3f5e669675c
Callum Asplen <casplen@outlook.com>
2020-11-18 20:05:28 +0000

    Added isogram finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,17 @@
 const IsogramFinder = function (word) {
+    this.word = word;
 
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    let lowerCase = this.word.toLowerCase()
+    let letterArray = lowerCase.split('')
+    
+    if (letterArray.some((letter, index) => letterArray.indexOf(letter) !== index)) {
+        return false
+    } else {
+        return true
+    }
 }
 
 module.exports = IsogramFinder;
